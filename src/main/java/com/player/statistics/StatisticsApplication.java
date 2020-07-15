30446911754b78d3403acc75840f4b185f4bef07
Purushottam Singh <purushottamsingh@PurushottamsMBP.home>
2020-07-15 12:58:04 -0400

    inittial checkin

---
+++ src/main/java/com/player/statistics/StatisticsApplication.java
@@ -0,0 +1,13 @@
+package com.player.statistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatisticsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/player/statistics/controllers/IndexController.java
@@ -0,0 +1,10 @@
+package com.player.statistics.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class IndexController {
+ public String getIndex(){
+    return null;
+ }   
+}
